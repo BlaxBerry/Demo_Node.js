e4c9f785a81a7ea4d99f40c36a11d1c30b299f8c
Chen <chenjiaxu333@gmail.com>
2021-02-16 21:37:43 +0900

    respod http status and content-type

---
+++ Node/6. respond write headers/server.js
@@ -0,0 +1,37 @@
+const http = require('http');
+
+const app = http.createServer();
+app.on('request', (req, res) => {
+
+    // 状态码默认是200
+    // 200  request succeed
+    // 404  not found
+    // 505  server error 服务器出问题
+    // 400  request error 客户端请求错误
+    // res.writeHead(200);
+    // res.writeHead(404);
+    // res.writeHead(505);
+    // res.writeHead(400)
+
+    // 响应内容类型
+    //content-type
+    // text/palin 纯文本
+    // text/html
+    // text/css
+    // image/jpeg
+    // application/json
+    // res.writeHead(400, {
+    //     'content-type': 'text/plain'
+    // });
+    res.writeHead(200, {
+        'content-type': 'text/html;charset=utf8'
+    });
+    res.end(`<h4>i am respond content</h4>
+            <h4>i am html tag</h4>
+    `)
+
+});
+
+app.listen(3000);
+
+console.log('http server running at 127.0.0.1:3000');
