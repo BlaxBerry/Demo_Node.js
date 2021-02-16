f5d98a2b1fb6686b2d4fdce177ddd22215ba6fee
Chen <chenjiaxu333@gmail.com>
2021-02-16 20:06:33 +0900

    POST or GET

---
+++ Node/2. request method GET, POST/server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const app = http.createServer();
+app.on('request', (req, res) => {
+
+    console.log(req.method);
+
+    res.end(`request method is : ${req.method}`)
+});
+
+app.listen(3000);
+console.log('http server running at 127.0.0.1:3000');
